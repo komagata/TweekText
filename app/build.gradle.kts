6d66d6867e1806bb71f23aa46d4a1e6021577ff7
Masaki Komagata <komagata@gmail.com>
2026-05-30 23:25:21 +0900

    Update launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "jp.komagata.tweektext"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
